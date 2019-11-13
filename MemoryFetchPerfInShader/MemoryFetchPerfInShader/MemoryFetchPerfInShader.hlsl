a2baf16d9eeaef6ee026bbee9756378614f514d0
Xinghua Cao <xinghua.cao@intel.com>
2019-11-13 14:09:42 +0800

    Add MemoryFetchPerfInShader sample

---
+++ MemoryFetchPerfInShader/MemoryFetchPerfInShader/MemoryFetchPerfInShader.hlsl
@@ -0,0 +1,20 @@
+#define TS 32
+Texture2D<uint4> matrixA : register(t0);
+Texture2D<uint4> matrixB : register(t1);
+RWTexture2D<unorm float4> matrixC : register(u0);
+
+cbuffer randomRowCol : register(b0)
+{
+	uint randomRow;
+	uint randomCol;
+};
+
+[numthreads(TS, TS, 1)]
+void main(uint3 Gid : SV_GroupID, uint3 LocalId : SV_GroupThreadID)
+{
+	uint globalRow = randomRow + Gid.x * uint(32) + LocalId.x;
+	uint globalCol = randomCol + Gid.y * uint(32) + LocalId.y;
+	uint outputGlobalRow = Gid.x * uint(32) + LocalId.x;
+	uint outputGlobalCol = Gid.y * uint(32) + LocalId.y;
+	matrixC[uint2(outputGlobalRow, outputGlobalCol)] = (matrixA[uint2(globalRow, globalCol)] + matrixB[uint2(globalRow, globalCol)]) / float(512);
+}
---
+++ MemoryFetchPerfInShader/MemoryFetchPerfInShader/customtimer.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <Windows.h>
+
+class MandelTimer
+{
+private:
+	// for timing
+	LARGE_INTEGER startingTime, endingTime, elapsedMicroseconds;
+	LARGE_INTEGER frequency;
+
+public:
+	MandelTimer(void)
+	{
+		QueryPerformanceFrequency(&frequency);
+		Start();
+	}
+
+	void Start()
+	{
+		QueryPerformanceCounter(&startingTime);
+	}
+
+	// return elapsed time in microseconds since last start
+	LONGLONG ElapsedMicroseconds()
+	{
+		QueryPerformanceCounter(&endingTime);
+		elapsedMicroseconds.QuadPart = endingTime.QuadPart - startingTime.QuadPart;
+		elapsedMicroseconds.QuadPart *= 1000000;
+		elapsedMicroseconds.QuadPart /= frequency.QuadPart;
+
+		return elapsedMicroseconds.QuadPart;
+	}
+
+	LONGLONG Stop()
+	{
+		return ElapsedMicroseconds();
+	}
+};
